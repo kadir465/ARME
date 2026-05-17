14a471d9546a2b6baf8489770d7185ac67004d3c
kadir465 <kadiremiryucel123@gmail.com>
2026-05-18 01:02:37 +0300

    Fix deployment: Track env.js for GitHub Pages

---
+++ sdg5-website/env.js
@@ -0,0 +1,5 @@
+// ARME Projesi Yapılandırma Dosyası
+// Bu dosya hassas veriler içerir. Git'e gönderilmemelidir.
+const ARME_CONFIG = {
+    GEMINI_API_KEY: "AIzaSyAu8wQfqwKIjpLU5d7SWFHJvezkcX0qoW8"
+};
