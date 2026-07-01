3990c17ca0b17cd2761a267468f53ed32beadb34
kadir emir yücel <kadiremiryucel123@gmail.com>
2026-07-01 21:52:56 +0300

    Delete sdg5-website/env.js

--- sdg5-website/env.js
+++
@@ -1,5 +0,0 @@
-// ARME Projesi Yapılandırma Dosyası
-// Bu dosya hassas veriler içerir. Git'e gönderilmemelidir.
-const ARME_CONFIG = {
-    GEMINI_API_KEY: "AIzaSyAu8wQfqwKIjpLU5d7SWFHJvezkcX0qoW8"
-};
